f66a361d51735109b124dac989923b46586748cd
Luka Fagundes <luka@sunny-stack.com>
2025-12-28 15:34:25 -0800

    Checkpoint

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ export default {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     '^ora$': '<rootDir>/tests/__mocks__/ora.ts',
     '^chalk$': '<rootDir>/tests/__mocks__/chalk.ts',
-    '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts'
+    '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts',
   },
   transform: {
     '^.+\\.tsx?$': [
@@ -21,22 +21,136 @@ export default {
           moduleResolution: 'nodenext',
           esModuleInterop: true,
           allowSyntheticDefaultImports: true,
-          isolatedModules: true
-        }
+          isolatedModules: true,
+        },
       },
     ],
   },
-  testMatch: [
-    '**/tests/**/*.test.ts',
-    '**/?(*.)+(spec|test).ts'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/index.ts'
-  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/index.ts'],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
+  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
   testTimeout: 30000, // 30 seconds for integration tests
-  verbose: true
+  verbose: true,
+  projects: [
+    {
+      displayName: 'unit',
+      preset: 'ts-jest/presets/default-esm',
+      testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^ora$': '<rootDir>/tests/__mocks__/ora.ts',
+        '^chalk$': '<rootDir>/tests/__mocks__/chalk.ts',
+        '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts',
+      },
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            useESM: true,
+            tsconfig: {
+              module: 'nodenext',
+              target: 'ES2022',
+              lib: ['ES2022'],
+              moduleResolution: 'nodenext',
+              esModuleInterop: true,
+              allowSyntheticDefaultImports: true,
+              isolatedModules: true,
+            },
+          },
+        ],
+      },
+      testMatch: ['**/tests/unit/**/*.test.ts'],
+    },
+    {
+      displayName: 'integration',
+      preset: 'ts-jest/presets/default-esm',
+      testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^ora$': '<rootDir>/tests/__mocks__/ora.ts',
+        '^chalk$': '<rootDir>/tests/__mocks__/chalk.ts',
+        '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts',
+      },
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            useESM: true,
+            tsconfig: {
+              module: 'nodenext',
+              target: 'ES2022',
+              lib: ['ES2022'],
+              moduleResolution: 'nodenext',
+              esModuleInterop: true,
+              allowSyntheticDefaultImports: true,
+              isolatedModules: true,
+            },
+          },
+        ],
+      },
+      testMatch: ['**/tests/integration/**/*.test.ts'],
+    },
+    {
+      displayName: 'e2e',
+      preset: 'ts-jest/presets/default-esm',
+      testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^ora$': '<rootDir>/tests/__mocks__/ora.ts',
+        '^chalk$': '<rootDir>/tests/__mocks__/chalk.ts',
+        '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts',
+      },
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            useESM: true,
+            tsconfig: {
+              module: 'nodenext',
+              target: 'ES2022',
+              lib: ['ES2022'],
+              moduleResolution: 'nodenext',
+              esModuleInterop: true,
+              allowSyntheticDefaultImports: true,
+              isolatedModules: true,
+            },
+          },
+        ],
+      },
+      testMatch: ['**/tests/e2e/**/*.test.ts'],
+    },
+    {
+      displayName: 'performance',
+      preset: 'ts-jest/presets/default-esm',
+      testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^ora$': '<rootDir>/tests/__mocks__/ora.ts',
+        '^chalk$': '<rootDir>/tests/__mocks__/chalk.ts',
+        '^inquirer$': '<rootDir>/tests/__mocks__/inquirer.ts',
+      },
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            useESM: true,
+            tsconfig: {
+              module: 'nodenext',
+              target: 'ES2022',
+              lib: ['ES2022'],
+              moduleResolution: 'nodenext',
+              esModuleInterop: true,
+              allowSyntheticDefaultImports: true,
+              isolatedModules: true,
+            },
+          },
+        ],
+      },
+      testMatch: ['**/tests/performance/**/*.test.ts'],
+    },
+  ],
 };
