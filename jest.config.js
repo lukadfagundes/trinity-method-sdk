a9e3c87eb1f965fd0fc23c16381322a51f968cf5
Luka Fagundes <luka@sunny-stack.com>
2026-02-24 16:19:38 -0800

    feat: release version v2.2.0

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,7 @@ export default {
         ],
       },
       testMatch: ['**/tests/integration/**/*.test.ts'],
-      testTimeout: 60000, // 60 seconds for integration tests (Windows CI needs more time)
+      testTimeout: 120000, // 120 seconds for integration tests (Windows CI needs more time)
     },
     {
       displayName: 'e2e',
